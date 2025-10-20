ebe4a7ca27ea64290bb5d6f03b439ae1a69e79e6
kiyaye1 <36045045+kiyaye1@users.noreply.github.com>
2025-10-20 09:13:31 -0400

    Update VehicleOopApplicationTests.java

--- src/test/java/com/synex/VehicleOopApplicationTests.java
+++ src/test/java/com/synex/VehicleOopApplicationTests.java
@@ -2,12 +2,10 @@ package com.synex;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class VehicleOopApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
+    @Test void contextLoads() {}
 }
