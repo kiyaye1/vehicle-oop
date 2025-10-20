f73f5c4683b4c027fbf03ecb8669cca7fdb06d49
kiyaye1 <36045045+kiyaye1@users.noreply.github.com>
2025-10-20 09:58:47 -0400

    Update vpc.tf

--- infra/vpc.tf
+++ infra/vpc.tf
@@ -25,7 +25,11 @@ resource "aws_subnet" "public_b" {
 
 resource "aws_route_table" "public_rt" {
   vpc_id = aws_vpc.main.id
-  route { cidr_block = "0.0.0.0/0" gateway_id = aws_internet_gateway.igw.id }
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
 }
 
 resource "aws_route_table_association" "a" {
