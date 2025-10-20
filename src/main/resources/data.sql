52b12760cd3f8dabd696387196de726c23c242e3
kiyaye1 <36045045+kiyaye1@users.noreply.github.com>
2025-10-20 15:25:55 -0400

    Create data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO VEHICLE (make, model, year) VALUES ('Toyota', 'Camry', 2020);
+INSERT INTO VEHICLE (make, model, year) VALUES ('Honda', 'Civic', 2021);
+INSERT INTO VEHICLE (make, model, year) VALUES ('Ford', 'Mustang', 2022);
