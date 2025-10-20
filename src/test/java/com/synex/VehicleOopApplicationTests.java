181b2e5d3909d530b258b0043da8ed39aa650cc9
kiyaye1 <36045045+kiyaye1@users.noreply.github.com>
2025-10-20 16:03:00 -0400

    Update VehicleOopApplicationTests.java

--- src/test/java/com/synex/VehicleOopApplicationTests.java
+++ src/test/java/com/synex/VehicleOopApplicationTests.java
@@ -3,9 +3,15 @@ package com.synex;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @ActiveProfiles("test")
-@SpringBootTest
+@TestPropertySource("classpath:test-db.properties")
+@SpringBootTest(properties = "spring.sql.init.mode=never")
 class VehicleOopApplicationTests {
-    @Test void contextLoads() {}
+
+    @Test
+    void contextLoads() {
+        System.out.println("VehicleOopApplication context loaded successfully using test-db.properties!");
+    }
 }
