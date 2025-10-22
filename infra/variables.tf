1979c30ced72a7bbed81780de5182654eb4cc730
kiyaye1 <36045045+kiyaye1@users.noreply.github.com>
2025-10-22 11:12:16 -0400

    Update variables.tf

--- infra/variables.tf
+++ infra/variables.tf
@@ -3,12 +3,12 @@ variable "region" {
   default = "us-east-2"
 }
 
-variable "cluster_name" { 
-    type    = string  
-    default = "vehicle-eks" 
+variable "cluster_name" {
+  type    = string
+  default = "vehicle-eks"
 }
 
-variable "vpc_cidr" { 
-    type    = string  
-    default = "10.0.0.0/16"
+variable "vpc_cidr" {
+  type    = string
+  default = "10.0.0.0/16"
 }
