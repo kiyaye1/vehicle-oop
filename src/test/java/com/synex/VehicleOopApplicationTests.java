14b9f4cc49053d829a996485ab14c333c2f4db8c
Eyob Abegaz <kiyaye1@gmail.com>
2025-10-25 14:47:16 -0400

    tests: disable contextLoads in CI

--- src/test/java/com/synex/VehicleOopApplicationTests.java
+++ src/test/java/com/synex/VehicleOopApplicationTests.java
@@ -1,17 +1,10 @@
 package com.synex;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.TestPropertySource;
 
-@ActiveProfiles("test")
-@TestPropertySource("classpath:test-db.properties")
-@SpringBootTest(properties = "spring.sql.init.mode=never")
+@Disabled("No full Spring context needed in CI")
 class VehicleOopApplicationTests {
-
-    @Test
-    void contextLoads() {
-        System.out.println("VehicleOopApplication context loaded successfully using test-db.properties!");
-    }
+  @Test void contextLoads() {}
 }
+
